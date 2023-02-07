a0afd67aae689dcc99187e391982b59486e97919
efs0-cod3 <eflearnng@gmail.com>
2023-02-07 18:14:08 -0400

    config file that holds .env and db file

--- api/index.js
+++ api/index.js
@@ -1,35 +1,44 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require('express')
+const mongoose = require("mongoose");
+const app = express()
+
+const connectDB = require('./config/database')
+
+
+const dotenv = require('dotenv').config({path: './config/.env'});
+
+// to avoid deprecation warning in mongoose 7
+mongoose.set("strictQuery", false);
+connectDB()
+
+const PORT = process.env.PORT || 3000
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+
+// variables creadas anteriormente.
 // const Ticket = require('./models/Ticket');
 
-// mongo db connection
-mongoose.connect(process.env.URI, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-
-    }, () => { console.log('Connected klk')}
-    );
 
 // push to master
-    let addTicket = (req, reply) => {
+    // let addTicket = (req, reply) => {
         
           
 
-          try {
+    //       try {
 
-            const ticket = new Ticket({ticket_name: "Grada", price: 1.52, description: "hola"})
-            return ticket.save()
+    //         const ticket = new Ticket({ticket_name: "Grada", price: 1.52, description: "hola"})
+    //         return ticket.save()
 
-            console.log("It's working");
+    //         console.log("It's working");
   
-          } catch (error)
-          {
+    //       } catch (error)
+    //       {
 
-            console.error(error);
-          }
+    //         console.error(error);
+    //       }
         
-      }
+    //   }
     
 
     /* testing
